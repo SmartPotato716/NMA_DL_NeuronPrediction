db0f4a0099828f47d6c95bdcade00cb799acbc00
Dario Cupolillo <dario.cupolillo@gmail.com>
2024-07-25 15:06:07 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import torch
+from motiondecoder import augment_trace
 
 
 def plot_loss_accuracy(
@@ -66,11 +67,86 @@ def plot_loss_accuracy(
     # Hyperparameters plot
     ax3.axis('off')
     hyperparams_text = '\n'.join([f'{key}: {value}'
-                                  for key, value in hyperparameters.items()])
+                                  for key, value in hyperparameters.items()
+                                  if key not in [
+                                          'train_loader',
+                                          'valid_loader',
+                                          'model',
+                                          'device',
+                                          'epochs']])
     ax3.text(
         0.5, 0.5,
         hyperparams_text,
         horizontalalignment='center',
         verticalalignment='center',
         fontsize=12)
+
+
+def augmentation_example_trace(
+        trace,
+        save: bool = False
+):
+
+    augmented_trace = augment_trace(trace, noise_level=0.1)
+
+    # Plot the original and augmented trace
+    fig, ax = plt.subplots(2, 1, figsize=(4, 3), sharex=True, sharey=True)
+
+    ax[0].plot(trace.numpy())
+    ax[1].plot(augmented_trace.numpy())
+
+    ax[1].set_xlabel('Time Bins')
+    ax[0].set_ylabel('Firing Rate (Hz)')
+
+    plt.tight_layout()
+
+    if save:
+        fig.savefig("figures/augmentation_example_trace.png")
+        print("Figure saved!")
+
+
+def plot_multiple_runs(
+        results,
+        to_plot='acc',
+        varying_param='lr',
+        save=False
+):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(6, 3), sharex=True)
+
+    for result in results:
+        params = result['params']
+        label = f"{params[varying_param]}"
+
+        if to_plot == 'acc':
+            ax1.plot(result['train_acc'], label=f"{label}")
+            ax2.plot(result['validation_acc'], label=f"{label}")
+            ax1.set_ylabel('Accuracy')
+            ax1.set_title(f'Train. across {varying_param}')
+            ax2.set_title(f'Valid. across {varying_param}')
+            ax2.set_ylim(0, 100)
+        elif to_plot == 'loss':
+            ax1.plot(result['train_loss'], label=f"{label}")
+            ax2.plot(result['validation_loss'], label=f"{label}")
+            ax1.set_ylabel('Loss')
+            ax1.set_title(f'Train. across {varying_param}')
+            ax2.set_title(f'Valid. across {varying_param}')
+        else:
+            raise ValueError("Invalid value for to_plot. Use 'acc' or 'loss'.")
+
+    ax1.set_xlabel('Epochs')
+    ax2.set_xlabel('Epochs')
+
+    # Creating a combined legend for both train and validation plots
+    handles, labels = ax1.get_legend_handles_labels()
+    ax1.legend(handles, labels)
+    handles, labels = ax2.get_legend_handles_labels()
+    ax2.legend(handles, labels)
+
+    plt.tight_layout()
+
+    if save:
+        # Generate filename based on to_plot and varying_param
+        filename = f"figures/{to_plot}_across_{varying_param}.png"
+        plt.savefig(filename)
+        print(f"Figure saved as {filename}")
 
